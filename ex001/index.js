702141255daa7bb3a6a6cb25438bcc60dd885f41
Thomas-Paz <thomaspaz08@gmail.com>
2024-05-06 14:18:03 -0300

    const

---
+++ ex001/index.js
@@ -0,0 +1,31 @@
+// Não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um numero
+// Não podem conter espaços ou traços
+// case-sensitive
+// não pode modificar o valor de uma constante
+// não utilize VAR, UTILIZE CONST.
+// dentro das aspas = string (texto), fora das aspas = number (numero)
+
+
+const nome = 'Thomas Paz';
+
+
+const idade = '21 anos'
+
+
+
+const peso = '77kg';
+
+
+
+const altura = '171cm'
+
+
+
+const firstnum = 16;
+const secondnum = 18;
+
+const resultado = firstnum / secondnum;
+
+console.log (typeof firstnum);
