3add343a7b5013fc718ed9631b79c3f25969c896
Thomas-Paz <thomaspaz08@gmail.com>
2024-05-09 18:16:25 -0300

    const

--- ex002/index.js
+++ ex002/index.js
@@ -1,9 +1,16 @@
 var nome = 'Thomas'; 
-console.log (nome);
+
 
 let nomeAluno; // undefined -> não aponta para local nenhum na memoria
 
 const sobrenomeAluno = null; // Nulo -> não aponta para local nenhuma na memoria
 
 const boolean = true; // or false; = const aprovado = true or false = valor "boolean" contém apenas 2 valores true ou false (lógico)
+
+const a = [1, 2];
+const b = a;
+
+console.log (a, b);
+
+b.push (3); 
 
